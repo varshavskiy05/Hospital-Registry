e6f59cbe4df246be51cd97b3ba9569ad784a369d
varshavskiy_png <timikinvarshav05@gmail.com>
2025-05-06 20:09:21 +0300

    fix

--- appointments/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import AppointmentSlot
-
-admin.site.register(AppointmentSlot)
--- appointments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'appointments'
-
-urlpatterns = [
-    path('', views.appointment_list, name='list'),
-
-]
